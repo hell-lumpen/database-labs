b16a84d44da371559bca3e67c801b5136afc09d1
Grubenko Maxim <mdgrubenko@maieducation>
2022-11-14 14:50:14 +0300

    creating tables and filling

---
+++ init_tables.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS programs (
+    id SERIAL PRIMARY KEY,
+    program_name VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS events (
+    id SERIAL PRIMARY KEY,
+    event_name VARCHAR(70) NOT NULL,
+    start_time TIME NOT NULL,
+    duration TIME NOT NULL,
+    program_id INT NOT NULL REFERENCES programs(id)
+);
+
+CREATE TABLE IF NOT EXISTS participants (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS participants_of_events (
+    participant_id INT REFERENCES participants(id) NOT NULL ,
+    event_id INT REFERENCES events(id) NOT NULL ,
+    PRIMARY KEY (participant_id, event_id)
+);
+
+CREATE TABLE IF NOT EXISTS companies (
+    id SERIAL PRIMARY KEY,
+    company_name VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS organizers (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    company_id INT NOT NULL REFERENCES companies(id)
+);
+
+CREATE TABLE IF NOT EXISTS organizers_of_events (
+    organizer_id INT REFERENCES organizers(id) NOT NULL ,
+    event_id INT REFERENCES events(id) NOT NULL ,
+    PRIMARY KEY (organizer_id, event_id)
+);
